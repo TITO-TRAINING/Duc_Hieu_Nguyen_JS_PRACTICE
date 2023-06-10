ef64eca460a191535442e09275e9ff83e94447dd
Nguyen Duc Hieu <dhieu03@gmail.com>
2023-06-10 23:35:45 +0700

    create view modules

---
+++ src/views/modules/ActionBar.js
@@ -0,0 +1,13 @@
+function ActionBar() {
+  return `
+    <div class="top-bar">
+      <input type="text" name="search" id="search-box" placeholder="Search..">
+      <button class="add-btn">
+        <i class="ti-plus"></i>
+        Add new Book
+      </button>
+    </div>
+  `;
+}
+
+export default ActionBar;
---
+++ src/views/modules/BookTable.js
@@ -0,0 +1,24 @@
+function BookTable() {
+  return `
+    <table>
+      <thead>
+        <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Category</th>
+            <th>Status</th>
+            <th>Number</th>
+            <th>Price($)</th>
+            <th>Check</th>
+            <th>Update</th>
+            <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+    </table>
+  `;
+}
+
+export default BookTable;
