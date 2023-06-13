d4cd81855d5f7d609b936c5e1c60785397b8ad71
Nguyen Duc Hieu <dhieu03@gmail.com>
2023-06-13 18:33:53 +0700

    create modal module

--- src/views/book.view.js
+++ src/views/book.view.js
@@ -1,27 +1,21 @@
 import BookTable from './modules/BookTable';
 import ActionBar from './modules/ActionBar';
+import BookModal from './modules/BookModal';
 
 class BookView {
   constructor() {
-    this.app = this.getElement('#root');
-    this.app.innerHTML = ActionBar;
-    this.container = this.createElement('div', 'container');
-    this.container.innerHTML = BookTable;
+    this.app = document.querySelector('#root');
+    this.app.innerHTML = ActionBar();
+    this.app.innerHTML += BookModal({});
 
-    this.table = this.getElement('.book-list');
+    this.container = document.createElement('div');
+    this.container.classList.add('container');
+    this.container.innerHTML = BookTable();
+
+    this.table = document.querySelector('.book-list');
 
     // add modules
     this.app.append(this.container);
-  }
-
-  static createElement(tag, className) {
-    const element = document.createElement(tag);
-    if (className) element.classList.add(className);
-    return element;
-  }
-
-  static getElement(selector) {
-    return document.querySelector(selector);
   }
 }
 
---
+++ src/views/modules/BookModal.js
@@ -0,0 +1,82 @@
+function BookModal({
+  id = null,
+  title = null,
+  author = null,
+  category = null,
+  status = false,
+  number = null,
+  price = null,
+}) {
+  return `
+    <div class="modal hidden">
+      <div class="close-btn">
+        <i class="ti-close"></i>
+      </div>
+      <form class="book-form">
+        <input type="hidden" id="book-id" value="${id}">
+        <label for="book-title">Title</label>
+        <input
+        type="text"
+        name="book-title"
+        id="book-title"
+        value="${title || ''}"
+        >
+
+        <label for="book-author">Author</label>
+        <input
+        type="text"
+        name="book-author"
+        id="book-author"
+        value="${author || ''}"
+        >
+
+        <label for="book-category">Category</label>
+        <input
+        type="text"
+        name="book-category"
+        id="book-category"
+        value="${category || ''}"
+        >
+
+        <div class="status-wrap">
+          <label for="active">Active</label>
+          <input
+          ${status ? 'checked' : ''}
+          type="radio"
+          name="book-category"
+          value="active" id="active"
+          class="status-btn active-btn">
+
+          <label for="inactive">Inactive</label>
+          <input
+          ${!status ? 'checked' : ''}
+          type="radio"
+          name="book-category"
+          value="inactive"
+          id="inactive"
+          class="status-btn inactive-btn">
+        </div>
+
+
+        <label for="book-number">Quantity</label>
+        <input
+        type="number"
+        name="book-number"
+        id="book-number"
+        value="${number || ''}"
+        >
+
+        <label for="book-price">Price</label>
+        <input
+        type="text"
+        name="book-price"
+        id="book-price"
+        value="${price || ''}"
+        >
+
+        <button class="save-btn">Save</button>
+      </form>
+    </div>`;
+}
+
+export default BookModal;
